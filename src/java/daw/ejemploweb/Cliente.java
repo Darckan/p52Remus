5db4f42db9eadb35ab06b9d3283228ce9af2148e
Darckan <remushurmuz4@gmail.com>
2019-02-01 13:45:38 +0100

    edit Cliente

--- src/java/daw/ejemploweb/Cliente.java
+++ src/java/daw/ejemploweb/Cliente.java
@@ -4,5 +4,41 @@ public class Cliente {
     private String nombre;
     private String email;
     private int edad;
+
+    public Cliente(String nombre, String email, int edad) {
+        this.nombre = nombre;
+        this.email = email;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "nombre=" + nombre + ", email=" + email + ", edad=" + edad + '}';
+    }
+    
     
 }
